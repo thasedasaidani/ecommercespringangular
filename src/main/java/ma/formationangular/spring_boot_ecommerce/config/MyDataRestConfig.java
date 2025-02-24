2ced6dde3ccf6401628fa1ee7b2eee26bed73c4e
thaseda saidani <thasedasaidani2000@gmail.com>
2025-02-24 17:20:16 +0100

    Configure our REST API as read-only

--- src/main/java/ma/formationangular/spring_boot_ecommerce/config/MyDataRestConfig.java
+++ src/main/java/ma/formationangular/spring_boot_ecommerce/config/MyDataRestConfig.java
@@ -2,6 +2,7 @@ package ma.formationangular.spring_boot_ecommerce.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.mapping.ExposureConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -14,6 +15,20 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+    	  HttpMethod[] unsupportedActions = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
+
+          // Désactiver les méthodes HTTP pour Product
+          config.getExposureConfiguration()
+                  .forDomainType(Product.class)
+                  .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                  .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+          // Désactiver les méthodes HTTP pour ProductCategory
+          config.getExposureConfiguration()
+                  .forDomainType(ProductCategory.class)
+                  .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                  .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
         config.setBasePath("/api");  // Laissez juste la configuration de base
         config.exposeIdsFor(Product.class, ProductCategory.class); // Expose les IDs pour Product et ProductCategory
     }
